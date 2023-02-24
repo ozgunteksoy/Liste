e3e83e07ce0f79e71c8daecf135a72f7965f6373
OZGUN <ozgunteksoy@gmail.com>
2023-02-24 21:59:17 +0300

    Added a Procfile.

--- app.js
+++ app.js
@@ -143,6 +143,13 @@ app.get("/about", function (req, res) {
   res.render("about");
 });
 
-app.listen(3000, function () {
-  console.log("Server started on port 3000");
+
+// Heroku //
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+
+app.listen(port, function () {
+  console.log("Server has started on port 3000");
 });
